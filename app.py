b520bf68cac0a01abc2d8227d60c3414055a287d
Dhaval Modi <dhavalmodi24@gmail.com>
2017-07-11 15:25:23 +0530

    commit 3

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+@app.route('/image')
+def speech():
+    return redirect(url_for('html', filename='index.html'))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
