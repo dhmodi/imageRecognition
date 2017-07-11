6ad7443355610cf618ec036683f51d04ad955e9e
Dhaval Modi <dhavalmodi24@gmail.com>
2017-07-11 15:26:32 +0530

    commit 3

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import gc
 gc.collect()
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
